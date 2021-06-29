f1c87ee1a087838224aad305f2f8497c11b53523
Roman Mazeev <romanmazeev@icloud.com>
2021-06-29 15:45:40 +0300

    Add tests for visas

--- Tests/MRZParserTests/MRZParserTests.swift
+++ Tests/MRZParserTests/MRZParserTests.swift
@@ -2,6 +2,8 @@
     @testable import MRZParser
 
     final class MRZParserTests: XCTestCase {
+        private var parser: MRZParser!
+    
         private let dateFormatter: DateFormatter = {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyyMMdd"
@@ -10,8 +12,13 @@
             return formatter
         }()
 
+        override func setUp() {
+            super.setUp()
+
+            parser = MRZParser()
+        }
+
         func testTD1() {
-            let parser = MRZParser()
             let mrzString = """
                             I<UTOD231458907<<<<<<<<<<<<<<<
                             7408122F1204159UTO<<<<<<<<<<<6
@@ -40,7 +47,6 @@
         }
 
         func testTD2() {
-            let parser = MRZParser()
             let mrzString = """
                             I<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<
                             D231458907UTO7408122F1204159<<<<<<<6
@@ -68,7 +74,6 @@
         }
 
         func testTD3() {
-            let parser = MRZParser()
             let mrzString = """
                             P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<
                             L898902C36UTO7408122F1204159ZE184226B<<<<<10
@@ -95,12 +100,58 @@
             XCTAssertEqual(parser.parse(mrzString: mrzString), result)
         }
 
-        private func date(from string: String) -> Date {
-            let dateStringFormatter = DateFormatter()
-            dateStringFormatter.dateFormat = "yyyy-MM-dd"
-            dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale
-            return dateStringFormatter.date(from: string)!
+        func testMRVA() {
+            let mrzString = """
+                            V<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<
+                            L8988901C4XXX4009078F96121096ZE184226B<<<<<<
+                            """
+            let result = MRZResult(
+                documentType: "V",
+                countryCode: "UTO",
+                surnames: "ERIKSSON",
+                givenNames: "ANNA MARIA",
+                documentNumber: "L8988901C",
+                nationalityCountryCode: "XXX",
+                birthdate:  dateFormatter.date(from: "19400907"),
+                sex: "FEMALE",
+                expiryDate: dateFormatter.date(from: "19961210"),
+                personalNumber: "6ZE184226B",
+                personalNumber2: nil,
+                isDocumentNumberValid: true,
+                isBirthdateValid: true,
+                isExpiryDateValid: true,
+                isPersonalNumberValid: nil,
+                allCheckDigitsValid: true
+            )
+
+            XCTAssertEqual(parser.parse(mrzString: mrzString), result)
+        }
+
+        func testMRVB() {
+            let parser = MRZParser()
+            let mrzString = """
+                            V<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<
+                            L8988901C4XXX4009078F9612109<<<<<<<<
+                            """
+            let result = MRZResult(
+                documentType: "V",
+                countryCode: "UTO",
+                surnames: "ERIKSSON",
+                givenNames: "ANNA MARIA",
+                documentNumber: "L8988901C",
+                nationalityCountryCode: "XXX",
+                birthdate:  dateFormatter.date(from: "19400907"),
+                sex: "FEMALE",
+                expiryDate: dateFormatter.date(from: "19961210"),
+                personalNumber: "",
+                personalNumber2: nil,
+                isDocumentNumberValid: true,
+                isBirthdateValid: true,
+                isExpiryDateValid: true,
+                isPersonalNumberValid: nil,
+                allCheckDigitsValid: true
+            )
+
+            XCTAssertEqual(parser.parse(mrzString: mrzString), result)
         }
     }
-
-
